4a9b46207c483b79f90784994ee4a2d575a5b38a
Gerardo Merida <cgemerida@gmail.com>
2019-06-29 09:46:30 -0600

    Angular route basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,22 @@ import { MatButtonModule } from "@angular/material/button";
 import { AppComponent } from './app.component';
 
 import "hammerjs";
+
+// MY SERVICES
+import { DishService } from "./services/dish.service";
+
+// MY COMPONENTS
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from "./services/dish.service";
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+
+
 
 @NgModule({
   declarations: [
@@ -25,7 +35,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +48,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [
     DishService 
